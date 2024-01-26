a92a18a98b2b06cea2bef35699612758c6ddb82c
Chavez Harris <codedbychavez@gmail.com>
2024-01-26 11:40:00 -0300

    add hook usage with composabale

--- sample-app-nightly/src/views/HomeView.vue
+++ sample-app-nightly/src/views/HomeView.vue
@@ -16,10 +16,32 @@
 </template>
 
 <script setup lang="ts">
+import { onBeforeMount, onMounted, ref } from "vue";
 import TheWelcome from "../components/TheWelcome.vue";
 import TheNewFeature from "@/components/TheNewFeature.vue";
-import { FeatureWrapper } from "configcat-vue";
-// import NightlyFeatureWrapper from "@/components/NightlyFeatureWrapper.vue";
+import { FeatureWrapper, type SettingKeyValue } from "configcat-vue";
+
+// using the underlying configcat client
+// import the composable
+import { useConfigCatClient } from "configcat-vue";
+// use the composable
+const configCatClient = useConfigCatClient();
+
+const featureFlags = ref<SettingKeyValue[] | undefined>([]);
+
+onBeforeMount(() => {
+    // using hooks: Subscribe to the hook using the .on method of the ConfigCat client
+    configCatClient?.on('flagEvaluated', () => {
+    console.log('Flag evaluated')
+  })
+});
+
+onMounted(async () => {
+  // Fetch feature flags
+  const response = await configCatClient.getAllValuesAsync();
+  featureFlags.value = response;
+  console.log(featureFlags.value);
+});
 </script>
 
 <style scoped>
