e10d41ec002464205e386e446fb8be1638080f48
Chavez Harris <codedbychavez@gmail.com>
2024-01-25 21:37:54 -0300

    init sample app

---
+++ sample-app-nightly/src/components/TheNewFeature.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="the-new-feature">
+        I am the new feature
+    </div>
+</template>
+
+<style scoped>
+
+.the-new-feature {
+    background-color: lemonchiffon;
+    border: 1px solid gray;
+    padding: .5rem;
+}
+
+</style>
--- sample-app-nightly/src/views/HomeView.vue
+++ sample-app-nightly/src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
+    <div class="the-new-feature-wrapper">
+      <TheNewFeature />
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+import TheNewFeature from "@/components/TheNewFeature.vue";
+</script>
+
+<style scoped>
+
+.the-new-feature-wrapper {
+  margin-top: 2rem;
+}
+
+</style>
