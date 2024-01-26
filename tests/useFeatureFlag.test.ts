0ddf468e95415de75a9459c37cf53e0be67f148c
Chavez Harris <codedbychavez@gmail.com>
2024-01-26 19:52:10 -0300

    ready for cc client mocking

--- tests/useFeatureFlag.test.ts
+++ tests/useFeatureFlag.test.ts
@@ -1,14 +1,33 @@
 import { test, expect } from "vitest";
+import { defineComponent } from "vue";
+import { useFeatureFlag } from "../src/composables/useFeatureFlag";
+import { mockConfigCatClient } from "./mocks/mockConfigCatClient";
+import { mount } from "@vue/test-utils";
 
 test("Evaluates a feature flag", () => {
   // 1. define a test component that will use this composable
   // in the setup context
 
-  // 2. create a variable in the template {{ featureFlagValue }}
+  const TestComponent = defineComponent({
+    setup() {
+      const { isFeatureFlagEnabled } = useFeatureFlag("myFirstFeatureFlag");
 
+      return {
+        isFeatureFlagEnabled,
+      };
+    },
+    template: "<div>{{ featureFlagValue }}</div>",
+  });
 
-  // 2. provide the mock configcat client in the global scope
-  // the client should have the getValueAsync method and return
+  const wrapper = mount(TestComponent, {
+    // mock what main.ts in a vue app is doing
+    global: {
+      provide: {
+        // hint: this is the source of truth, the useFeatureFlag composable will use this
+        configCatClient: mockConfigCatClient,
+      },
+    },
+  });
 
   // 3. the mock configcat client getValueAsync fake implementation will always return the default value provide
   // may need to await the result
@@ -16,6 +35,4 @@ test("Evaluates a feature flag", () => {
   // 4. mount the test component using the wrapper
 
   // 5. test if the text in the test component is = to the default value passed in getValueAsync
-
-
-})
+});
