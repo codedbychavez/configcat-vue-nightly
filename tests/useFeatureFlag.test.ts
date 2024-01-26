61129b4c1ae2dbcc138f8095be9912cdc2f37553
Chavez Harris <codedbychavez@gmail.com>
2024-01-26 19:16:44 -0300

    init stuff

---
+++ tests/useFeatureFlag.test.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "vitest";
+
+test("Evaluates a feature flag", () => {
+  // 1. define a test component that will use this composable
+  // in the setup context
+
+  // 2. create a variable in the template {{ featureFlagValue }}
+
+
+  // 2. provide the mock configcat client in the global scope
+  // the client should have the getValueAsync method and return
+
+  // 3. the mock configcat client getValueAsync fake implementation will always return the default value provide
+  // may need to await the result
+
+  // 4. mount the test component using the wrapper
+
+  // 5. test if the text in the test component is = to the default value passed in getValueAsync
+
+
+})
