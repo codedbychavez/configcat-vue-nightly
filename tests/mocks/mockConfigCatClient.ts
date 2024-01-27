f81e636a3e975413858aef40a6c495f3b059e2f7
Chavez Harris <codedbychavez@gmail.com>
2024-01-27 10:11:10 -0300

    mock cc client

--- tests/mocks/mockConfigCatClient.ts
+++ tests/mocks/mockConfigCatClient.ts
@@ -1,3 +1,21 @@
-import { IConfigCatClient } from "configcat-common";
+import {
+  IConfigCatClient,
+  SettingTypeOf,
+  SettingValue,
+  User,
+} from "configcat-common";
 
-export const mockConfigCatClient = {} as IConfigCatClient;
+class MockConfigCatClient {
+  getValueAsync<T extends SettingValue>(
+    key: string,
+    defaultValue: T,
+    user?: User | undefined
+  ): Promise<SettingTypeOf<T>> {
+    // Do something with the key, and user
+    const details = { key, user, defaultValue };
+    return Promise.resolve(details.defaultValue as SettingTypeOf<T>);
+  }
+}
+
+export const mockConfigCatClient =
+  new MockConfigCatClient() as IConfigCatClient;
